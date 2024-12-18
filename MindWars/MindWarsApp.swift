58893a406b43dc348c4c1d82f6b86f978d89446a
Yiğit TİLKİ <yigittilkiw@gmail.com>
2024-12-18 21:22:50 +0300

    Initial Commit

---
+++ MindWars/MindWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindWarsApp.swift
+//  MindWars
+//
+//  Created by Yiğit Tilki on 18.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MindWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
